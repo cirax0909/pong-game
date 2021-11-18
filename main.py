aea17d4a74fbf11ad6a125eb2439711f463a2d70
cirax0909 <62638678+cirax0909@users.noreply.github.com>
2021-11-18 17:19:36 -0500

    add ball class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 import time
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.title("Pong Game")
@@ -9,6 +10,7 @@ screen.bgcolor("black")
 screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -18,5 +20,8 @@ game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
+    ball.move()
+    if ball.ycor() > 300 or ball.ycor() < -300:
+        ball.bounce()
 
 screen.exitonclick()
