c7348b056da4e3d1e9ae932370ff3a4487cc1d3b
cirax0909 <62638678+cirax0909@users.noreply.github.com>
2021-11-18 13:49:02 -0500

    add paddle class

--- main.py
+++ main.py
@@ -1,33 +1,18 @@
 from turtle import Turtle, Screen
 import time
+from paddle import Paddle
 
 screen = Screen()
 screen.title("Pong Game")
 screen.screensize(canvwidth=800, canvheight=600)
 screen.bgcolor("black")
 screen.tracer(0)
-
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.penup()
-paddle.shapesize(5, 1)
-paddle.setpos(350, 0)
-screen.update()
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
 
 game_on = True
 while game_on:
